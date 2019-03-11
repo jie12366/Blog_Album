dd6c0bdfd1d0123a5a3144bf0f435ab7d43215bd
= <=>
2019-03-11 19:48:58 +0800

    add photos

--- gitPush.py
+++ gitPush.py
@@ -4,4 +4,4 @@ import os
 os.system('git remote add origi git@106.12.209.121:jie12366/blog_album.git')
 os.system('git add --all')
 os.system('git commit -m "add photos"')
-os.system('git push -u origin master')
+os.system('git push -u origi master')

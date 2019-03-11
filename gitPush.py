d74df141bfeac5da1ec8e6c5d8f6dad54197a44c
= <=>
2019-03-11 19:47:10 +0800

    add photos

--- gitPush.py
+++ gitPush.py
@@ -1,5 +1,7 @@
 #用python脚本执行git命令，提交到github仓库
 import os
+
+os.system('git remote add origi git@106.12.209.121:jie12366/blog_album.git')
 os.system('git add --all')
 os.system('git commit -m "add photos"')
 os.system('git push -u origin master')
